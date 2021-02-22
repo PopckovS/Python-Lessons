7156a1bf9fbaf3875223f2550113c21e1f5c6776
PopckovS <popckovM5@yandex.com>
2021-02-22 06:57:31 +0300

    Добавил решенеи мелких задачь

--- [@PumpTheMind]Jose Portilla[Udemy]/programm/Мелкие программы/1.py
+++ [@PumpTheMind]Jose Portilla[Udemy]/programm/Мелкие программы/1.py
@@ -16,7 +16,8 @@ def func1(cross=5):
     for i in list_result:
         print(i)
 
-def func2(list_one, list_two, mod=0):
+def func2(list_one, list_two):
+    """Найти пересечение списков"""
     if (type(list_one) is not list) or (type(list_two) is not list):
         print('Оба аргумента должны быть списками')
     elif (len(list_one) == 0) or (len(list_two) == 0):
@@ -24,7 +25,7 @@ def func2(list_one, list_two, mod=0):
 
     list_result = []
 
-    # Первый способ, нестрогое и строгое на основе аргумента mod
+    # Поиск пересечений списков
     for elem_one in list_one:
         for elem_two in list_two:
             if elem_one == elem_two:
@@ -33,14 +34,104 @@ def func2(list_one, list_two, mod=0):
     # Вывод получившегося списка
     print(list_result)
 
+def func3(file_name):
+    """ Напишите программу, которая принимает имя файла и выводит его расширение.
+    Если расширение у файла определить невозможно, выбросите исключение.
+    """
+    try:
+        if type(file_name) is not str:
+            raise TypeError('Аргумент должен быть строкой.')
+        file_split = file_name.split('.')
+        if len(file_split) < 2:
+            raise ValueError('Расширения файла не найдено.')
+    except ValueError:
+        print('У файла должно быть расширение')
+    except TypeError:
+        print('Аргумент должен быть строкой')
+    else:
+        print('Расширение файла = "{type}"'.format(type=file_split[-1]))
+
+
+def func4():
+    class Arithmetic():
+        """Класс для выполнения арифметических операций"""
+
+        def __init__(self, first_arg, second_arg, operation):
+            self.operations = ('+', '-', '*', '/')
+            self.tuple_arg = self.CheckArguments(first_arg, second_arg)
+            self.operation = self.CheckOperation(operation)
+            self.showResult(self.ExecuteOperation(self.tuple_arg, self.operation))
+
+        def showResult(self, result):
+            print("{one} {operation} {two} = {result}".format(
+                one=self.tuple_arg[0],
+                two=self.tuple_arg[1],
+                operation=self.operation,
+                result=result)
+            )
+
+        def ExecuteOperation(self, tuple_arg, operation):
+            if operation == '+':
+                return tuple_arg[0] + tuple_arg[1]
+            elif operation == '-':
+                return tuple_arg[0] - tuple_arg[1]
+            elif operation == '*':
+                return tuple_arg[0] * tuple_arg[1]
+            elif operation == '/':
+                return tuple_arg[0] / tuple_arg[1]
+
+        def CheckOperation(self, operation):
+            """Проверка является ли операция одной из разрешенных"""
+            try:
+                if operation in self.operations:
+                    return operation
+                else:
+                    raise ValueError('Неизвестная операция')
+            except:
+                raise
+
+        def CheckArguments(self, first_arg, second_arg):
+            """Проверка аргументов на числа или строки которые можно привести к числам."""
+            try:
+                first_arg = int(first_arg)
+                second_arg = int(second_arg)
+            except ValueError:
+                print('Аргументы должны быть числом или строкой которую можно привести к числу.')
+            else:
+                return (first_arg, second_arg)
+
+    at = Arithmetic('1', '5', '+')
+
+
+def func5():
+    class Bank():
+        """
+            Пользователь делает вклад в размере a рублей сроком на years лет
+        под 10% годовых (каждый год размер его вклада увеличивается на 10%.
+        Эти деньги прибавляются к сумме вклада, и на них в следующем году тоже
+        будут проценты).
+            Написать класс bank, принимающая аргументы a и years, и возвращающую
+        сумму, которая будет на счету пользователя.
+        """
+
+        def __init__(self):
+            pass
+
+
+    bank = Bank()
+
+
+# Запуск функций в работу
 # func1()
-func2(
-    # list_one=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
-    list_two=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
-    # list_two=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    list_one=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    mod=0
-)
 
+# func2(
+#     list_one=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
+#     list_two=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+# )
 
+# func3('Название файла.py')
+# func3('Название файла')
 
+# func4()
+
+func5()
