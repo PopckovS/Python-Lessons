2b14a72e480611835f0837333fbe31bfa588af28
PopckovS <popckov5@yandex.ru>
2021-07-22 21:33:18 +0300

    Добавил функции для работы с CSV как со словарями и списками.

--- Example/Practice_1/CSV/CSV.py
+++ Example/Practice_1/CSV/CSV.py
@@ -74,8 +74,101 @@ def func_3():
         csv_reader(f_obj)
 
 
+def func_4():
+
+    def csv_dict_reader(file_obj):
+        """
+        Используя другой метод, csv.DictReader() получим данные из файла.
+        Каждая строка в файле представляет из себя словарь,де ключами
+        являются названия столбцов,
+        """
+        reader = csv.DictReader(file_obj, delimiter=',')
+        for line in reader:
+            print(line["first_name"]), # Вывод первого элемента новой строки
+            print(line["last_name"])   # Вывод второго элемента новой строки
+
+    with open("data.csv") as f_obj:
+        csv_dict_reader(f_obj)
+
+
+def func_5():
+    """Пишем CSV файл"""
+
+    def csv_writer(data, path):
+        """
+        Используя метод csv.writer() получим обьект writer и уже
+        используя его метод writer.writerow() запишим данные в файл.
+        """
+        with open(path, "w", newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            for line in data:
+                writer.writerow(line)
+
+    # В результате получим список со списками
+    data = ["first_name,last_name,city".split(","),
+            "Tyrese,Hirthe,Strackeport".split(","),
+            "Jules,Dicki,Lake Nickolasville".split(","),
+            "Dedric,Medhurst,Stiedemannberg".split(",")
+            ]
+
+    path = "output.csv"
+    csv_writer(data, path)
+
+
+def func_6():
+    """
+    Используя метод DictWriter создадим список со словарями,
+    и запишим его в файл CSV
+    """
+    def csv_dict_writer(path, fieldnames, data):
+        """
+        Записываем в CSV файл данные используя DictWriter
+        """
+        with open(path, "w", newline='') as out_file:
+            writer = csv.DictWriter(out_file, delimiter=';', fieldnames=fieldnames)
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+
+    # Получим список со словарями
+    data = ["first_name,last_name,city".split(","),
+            "Tyrese,Hirthe,Strackeport".split(","),
+            "Jules,Dicki,Lake Nickolasville".split(","),
+            "Dedric,Medhurst,Stiedemannberg".split(",")
+            ]
+
+    # Финальный список что будет содержать все словари
+    my_list = []
+    # Список с первым рядом, в котором названия столбцов
+    fieldnames = data[0]
+    # Цикл проходится по всем спискам, создавая из них
+    # словари и добавляя их к финальному списку
+    for values in data[1:]:
+        # print(fieldnames, values)
+        inner_dict = dict(zip(fieldnames, values))  # Трансформ списка в словарь
+        # print('inner_dict = ', inner_dict)
+        my_list.append(inner_dict)  # Добавление словаря в финал список
+
+    path = "dict_output.csv"
+    csv_dict_writer(path, fieldnames, my_list)
+
+
+# Раскоментите чтобы выбрать нужный вариант
 # func_1()
 # func_2()
 # func_3()
+# func_4()
+# func_5()
+# func_6()
+
+
+
+
+
+
+
+
+
+
 
 
