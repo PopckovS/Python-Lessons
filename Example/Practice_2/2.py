d6e0b2426bd74f7b1517da67c8e411afea58dffa
Sergio <popckovM10@yandex.ru>
2022-03-12 23:42:37 +0300

    Практика с ООП в python

--- Example/Practice_2/2.py
+++ Example/Practice_2/2.py
@@ -79,6 +79,87 @@ def func3():
     # print(p2.__y)
 
 
+
+def func4():
+
+    class Point3():
+
+        def __init__(self):
+            pass
+
+        def getCoords(self):
+            return  self.__x, self.__y
+
+        def setCoords(self, x, y):
+            if (isinstance(x, int) or isinstance(x, float)) and \
+                    (isinstance(y, int) or isinstance(y, float)):
+                self.__x = x
+                self.__y = y
+            else:
+                print("Координаты должны быть числами")
+
+
+    pt3 = Point3()
+
+    pt3.setCoords(10, 20)
+    print(pt3.getCoords())
+
+    pt3.setCoords("10", 20)
+    print(pt3.getCoords())
+
+
+def func5():
+
+    class Point4():
+        static_count = 0
+
+        def __init__(self):
+            self.dinamic_x = 10
+
+
+    def show(x, title=''):
+        print('='*10, title, '='*10)
+        print('print = ', x)
+        print('type = ', type(x))
+
+    po = Point4()
+    ps = Point4
+
+    show(po, 'Обьект')
+    show(ps, 'Статический')
+
+
+def func6():
+    """
+    Можно использовать класс обращаясь к нему на прямую
+    """
+
+    class Point():
+        static_name = "Статическое название"
+        static_number = 10
+        def __init__(self, name):
+            self.dinamic_name = name
+
+    print(Point)
+    print(type(Point))
+
+
+    # Как видим создание любых переменных с ссылками на класс
+    # ведет к одному и томуже классу
+    p1 = Point
+    p2 = Point
+
+    print(p1.static_name)
+    print(p1.static_number)
+    p1.static_number = 20
+
+    print(p2.static_name)
+    print(p2.static_number)
+
+
 # func1()
 # func2()
-func3()
+# func3()
+# func4()
+# func5()
+func6()
