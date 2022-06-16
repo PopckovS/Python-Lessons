1f55db955a56aeaba56a30e8465522f446da0afb
Sergio <popckovM10@yandex.ru>
2022-06-16 23:50:48 +0300

    Добавил примеры ООП на python

--- Example/Practice_1/7.py
+++ Example/Practice_1/7.py
@@ -97,8 +97,159 @@ def func2():
     del pt.coordY
 
 
+def func3():
+    """
+    Второй способ, создания обьекта-свойства property
+    при помощи декораторов, данный метод дублирует код
+    и нарушает принцып DRY.
+    Данный способ неправильный, правильно это определять
+    класс Дескриптор, это позволит нне дублировать геттеры
+    сеттеры и удаляторы.
+    """
+    class Point:
+        def __init__(self, x, y):
+            self.__x = x
+            self.__y = y
+
+        # Метод для проверки вносимых значений на число
+        def __checkValueOnNumber(self, value):
+            if isinstance(value, int) or isinstance(value, float):
+                return True
+            return False
+
+        # ======= Методы для работы с атрибутом self.x =======
+        @property
+        def coordX(self):
+            print('Геттер coordX через декоратор')
+            return self.__x
+
+        @coordX.setter
+        def coordX(self, x):
+            print('Сеттер coordX через декоратор')
+            if self.__checkValueOnNumber(x):
+                self.__x = x
+            else:
+                raise ValueError('Неправильный формат данных')
+
+        @coordX.deleter
+        def coordX(self):
+            print('Удаление coordX через декоратор')
+            del self.__x
+
+        # ======= Методы для работы с атрибутом self.y =======
+        @property
+        def coordY(self):
+            print('Геттер coordY через декоратор')
+            return self.__y
+
+        @coordY.setter
+        def coordY(self, y):
+            print('Сеттер coordY через декоратор')
+            if self.__checkValueOnNumber(y):
+                self.__y = y
+            else:
+                raise ValueError('Неправильный формат данных')
+
+        @coordY.deleter
+        def coordY(self):
+            print('Удаление coordY через декоратор')
+            del self.__y
+
+    pt = Point(10, 20)
+
+    print('Использование X:')
+    pt.coordX = 100
+    print(pt.coordX)
+    del pt.coordX
+
+
+def func4():
+    """
+    Пример работы  Дескриптора
+    """
+
+    class CoordValue:
+        """Класс Дескриптора."""
+        # def __init__(self, name):
+        #     self.__name = name
+
+        def __set_name__(self, owner, name):
+            self.__name = name
+
+        def __get__(self, instance, owner):
+            return instance.__dict__[self.__name]
+
+        def __set__(self, instance, value):
+            instance.__dict__[self.__name] = value
+
+        def __delete__(self, instance):
+            del instance.__dict__[self.__name]
+
+    class Point:
+        """Класс для точки на плоскости, использует дескриптор"""
+        coordX = CoordValue()
+        coordY = CoordValue()
+
+        def __init__(self, x=0, y=0):
+            self.coordX = x
+            self.coordY = y
+
+    pt1 = Point()
+    pt2 = Point()
+
+    print('pt1.coordX = ', pt1.coordX, 'pt1.coordY = ', pt1.coordY)
+    print('pt2.coordX = ', pt2.coordX, 'pt2.coordY = ', pt2.coordY)
+
+    print('Вставка данных')
+    pt1.coordX = 10; pt1.coordY = 20
+    pt2.coordX = 100; pt2.coordY = 200
+
+    print('pt1.coordX = ', pt1.coordX, 'pt1.coordY = ', pt1.coordY)
+    print('pt2.coordX = ', pt2.coordX, 'pt2.coordY = ', pt2.coordY)
+
+    print('='*10)
+
+    print('pt1 = ', vars(pt1))
+    print('pt2 = ', vars(pt2))
+    del pt1.coordX
+    del pt2.coordX
+    print('pt1 = ', vars(pt1))
+    print('pt2 = ', vars(pt2))
+
+
+def func5():
+    """
+    Пример создания статического метода и его переопределение
+    """
+    class Point:
+        __count = 0
+
+        def __init__(self, x, y):
+            Point.__count += 1
+            self.x = x
+            self.y = y
+
+        @staticmethod
+        def getCounter():
+            return Point.__count
+
+    pt1 = Point(1, 2)
+    pt2 = Point(10, 20)
+
+    def newGetCounter():
+        return 55
+
+    pt1.getCounter = newGetCounter
+
+    print(pt1.getCounter())
+    print(Point.getCounter())
+
+
 # func1()
-func2()
+# func2()
+# func3()
+# func4()
+func5()
 
 
 
